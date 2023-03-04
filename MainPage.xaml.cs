e80fbba5cd3e5b9ade7f109e1f7c72896f75adad
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-03-04 22:20:48 +0600

    Going UP

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -20,5 +20,18 @@ public partial class MainPage : ContentPage
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+
+	private void OnDecClicked(object sender, EventArgs e)
+	{
+        count--;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }	
+
 }
 
